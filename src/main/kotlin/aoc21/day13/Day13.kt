05657c11496322cc1bb385c4c88ed503c82f0c5c
err87 <err87@cornell.edu>
2021-12-13 11:48:28 -0500

    day 13

---
+++ src/main/kotlin/aoc21/day13/Day13.kt
@@ -0,0 +1,95 @@
+package aoc21.day13
+
+import java.io.File
+import aoc21.utils.konsole.format
+import aoc21.utils.konsole.reset
+
+data class Instruction(val vertical: Boolean, val value: Int) {
+    val horizontal get() = !vertical
+    constructor(strings: List<String>): this(strings.first() == "x", strings.last().toInt())
+    constructor(string: String): this(string.substringAfterLast(' ').split('='))
+    fun applyTo(paper: Paper): Paper {
+//        print(paper)
+        val result = if (vertical) foldVertical(paper, value) else foldHorizontal(paper, value)
+//        result.print()
+        return result
+    }
+    fun print(paper: Paper) {
+        println()
+        paper.forEachIndexed { i, booleans ->
+            if (horizontal && i == value) "–".repeat(booleans.size)
+            else {
+                booleans.mapIndexed { j, b -> if (vertical && j == value) "|" else if (b) "#" else "." }.joinToString("")
+            }.let(::println)
+        }
+    }
+}
+
+data class Point(val x: Int, val y: Int) {
+    constructor(ints: List<Int>): this(ints[0], ints[1])
+    constructor(string: String): this(string.split(",").map(String::toInt))
+}
+
+typealias Paper = Array<Array<Boolean>>
+operator fun <T> Array<Array<T>>.get(i: Int, j: Int): T = this[i][j]
+operator fun <T> Array<Array<T>>.set(i: Int, j: Int, value: T) {
+    this[i][j] = value
+}
+
+fun Paper.print() {
+    println()
+    forEach { row ->  row.joinToString("") { if (it) "${(0x2588).toChar()}".format { bold; bright }.reset() else " " }.let(::println) }
+}
+
+
+fun fillPaper(points: List<Point>): Array<Array<Boolean>> {
+    val paper = Array(points.maxOf { it.y } + 1) { Array(points.maxOf { it.x } + 1) { false } }
+    points.forEach { (j, i) -> paper[i, j] = true }
+    return paper
+}
+
+fun foldHorizontal(paper: Paper, at: Int): Paper {
+    val height = if (at < paper.size / 2) paper.lastIndex - at else at
+    val width = paper.first().size
+    val result = Paper(height) { i ->
+        Array(width) { j ->
+            paper.getOrNull(at - height + i)?.get(j) ?: false || paper.getOrNull(at + height - i)?.get(j) ?: false
+        }
+    }
+    return result
+}
+
+fun foldVertical(paper: Paper, at: Int): Paper {
+    val width = if (at < paper.first().size / 2) paper.first().lastIndex - at else at
+    val result = Paper(paper.size) { i ->
+        Array(width) { j ->
+            paper[i].getOrNull(at - width + j) ?: false || paper[i].getOrNull(at + width - j) ?: false
+        }
+    }
+    return result
+}
+
+fun part1(paper: Paper, instruction: Instruction) = instruction.applyTo(paper).sumOf { row -> row.count { it } }
+
+fun part2(paper: Paper, instructions: List<Instruction>): Int {
+    val result: Paper = instructions.fold(paper) { acc, it -> it.applyTo(acc) }
+    result.print()
+    return 0
+}
+
+
+fun parseInput(inputFile: File): Pair<Paper, List<Instruction>> {
+    val (points, instructions) = inputFile.useLines { lines ->
+        lines.filter(String::isNotBlank).partition { it.first().isDigit() }
+    }
+    return fillPaper(points.map(::Point)) to instructions.map(::Instruction)
+}
+
+fun main() {
+    val inputFile = File("src/main/resources/Day13.txt")
+    val (paper, instructions) = parseInput(inputFile)
+    val result1 = part1(paper, instructions.first())
+    println(result1)
+    val result2 = part2(paper, instructions)
+
+}
---
+++ src/test/kotlin/aoc21/day13/Day13KtTest.kt
@@ -0,0 +1,39 @@
+package aoc21.day13
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+import java.io.File
+
+internal class Day13KtTest {
+
+    val inputFile = File("src/test/resources/Day13.txt")
+
+    @Test
+    fun part1() {
+        val (paper, instructions) = parseInput(inputFile)
+        val expect = 17
+        val actual = part1(paper, instructions.first())
+        assertEquals(expect, actual)
+    }
+
+    @Test
+    fun part1_2() {
+        val (paper, instructions) = parseInput(inputFile)
+        val expect = 16
+        val result = instructions.fold(paper) { acc, it -> it.applyTo(acc) }
+        assertEquals(7, result.size)
+        assertEquals(5, result.first().size)
+
+        val actual = result.sumOf { row -> row.count { it } }
+        assertEquals(expect, actual)
+    }
+
+    @Test
+    fun part2() {
+        val (paper, instructions) = parseInput(inputFile)
+        val expect = TODO()
+//        val actual = part2(parsedInput)
+//        assertEquals(expect, actual)
+    }
+}
