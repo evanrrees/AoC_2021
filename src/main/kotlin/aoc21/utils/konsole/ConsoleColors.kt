1cc6cd7dfaf9fbd664b9b3c1a62a8083bd90996c
err87 <err87@cornell.edu>
2021-12-16 12:39:05 -0500

    Experimenting with new interface - WIP

--- src/main/kotlin/aoc21/utils/konsole/ConsoleColors.kt
+++ src/main/kotlin/aoc21/utils/konsole/ConsoleColors.kt
@@ -2,7 +2,7 @@ package aoc21.utils.konsole
 
 enum class Face(val value: Int) { STANDARD(0), BOLD(1), UNDERLINED(4) }
 enum class Color(val value: Int?) {
-    RESET(null), BLACK(30), RED(31), GREEN(32), YELLOW(33), BLUE(34), PURPLE(35), CYAN(36), WHITE(37)
+    RESET(null), BLACK(30), RED(31), GREEN(32), YELLOW(33), BLUE(34), PURPLE(35), CYAN(36), WHITE(37), DEFAULT(39)
 }
 enum class Intensity(val value: Int) { STANDARD(0), BRIGHT(60) }
 enum class Plane(val value: Int) { FOREGROUND(0), BACKGROUND(10) }
@@ -47,79 +47,46 @@ class ElementBuilder {
 
 fun String.format(block: ElementBuilder.() -> Unit) = ElementBuilder().apply(block).build().plus(this)
 
-fun String.reset() = this + ElementBuilder().apply { color = Color.RESET }.build()
+//fun String.reset() = this + ElementBuilder().apply { color = Color.RESET }.build()
 
-//fun reset(): String = ElementBuilder().apply { color = Color.RESET }.build()
+const val ASCII_ESCAPE = '\u001b'
 
-//fun compose(block: ElementBuilder.() -> Unit): String {
-//    val builder = ElementBuilder()
-//    return builder.apply(block).build()
-//}
+fun String.bold()       = "$ASCII_ESCAPE[1m$this"
+fun String.dim()        = "$ASCII_ESCAPE[2m$this"
+fun String.underline()  = "$ASCII_ESCAPE[4m$this"
+fun String.blink()      = "$ASCII_ESCAPE[5m$this"
+fun String.invert()     = "$ASCII_ESCAPE[7m$this"
+fun String.hide()       = "$ASCII_ESCAPE[8m$this"
+fun String.defaultForeground() = "$ASCII_ESCAPE[39m$this"
+fun String.defaultBackground() = "$ASCII_ESCAPE[49m$this"
+fun String.reset()          = "$this$ASCII_ESCAPE[0m"
+fun String.resetBold()      = "$this$ASCII_ESCAPE[21m"
+fun String.resetDim()       = "$this$ASCII_ESCAPE[22m"
+fun String.resetUnderline() = "$this$ASCII_ESCAPE[24m"
+fun String.resetBlink()     = "$this$ASCII_ESCAPE[25m"
+fun String.resetInvert()    = "$this$ASCII_ESCAPE[27m"
+fun String.resetHide()      = "$this$ASCII_ESCAPE[28m"
 
-//fun foo() {
-//    val s = compose { bright; background; blue() }
-//}
-//
-//enum class ConsoleColors2(val value: String) {
-//    RESET("\u001b[0m"),
-//    BLACK("\u001b[0;30m"),
-//    RED("\u001b[0;31m"),
-//    GREEN("\u001b[0;32m"),
-//    YELLOW("\u001b[0;33m"),
-//    BLUE("\u001b[0;34m"),
-//    PURPLE("\u001b[0;35m"),
-//    CYAN("\u001b[0;36m"),
-//    WHITE("\u001b[0;37m"),
-//    BLACK_BOLD("\u001b[1;30m"),
-//    RED_BOLD("\u001b[1;31m"),
-//    GREEN_BOLD("\u001b[1;32m"),
-//    YELLOW_BOLD("\u001b[1;33m"),
-//    BLUE_BOLD("\u001b[1;34m"),
-//    PURPLE_BOLD("\u001b[1;35m"),
-//    CYAN_BOLD("\u001b[1;36m"),
-//    WHITE_BOLD("\u001b[1;37m"),
-//    BLACK_UNDERLINED("\u001b[4;30m"),
-//    RED_UNDERLINED("\u001b[4;31m"),
-//    GREEN_UNDERLINED("\u001b[4;32m"),
-//    YELLOW_UNDERLINED("\u001b[4;33m"),
-//    BLUE_UNDERLINED("\u001b[4;34m"),
-//    PURPLE_UNDERLINED("\u001b[4;35m"),
-//    CYAN_UNDERLINED("\u001b[4;36m"),
-//    WHITE_UNDERLINED("\u001b[4;37m"),
-//    BLACK_BACKGROUND("\u001b[40m"),
-//    RED_BACKGROUND("\u001b[41m"),
-//    GREEN_BACKGROUND("\u001b[42m"),
-//    YELLOW_BACKGROUND("\u001b[43m"),
-//    BLUE_BACKGROUND("\u001b[44m"),
-//    PURPLE_BACKGROUND("\u001b[45m"),
-//    CYAN_BACKGROUND("\u001b[46m"),
-//    WHITE_BACKGROUND("\u001b[47m"),
-//    BLACK_BRIGHT("\u001b[0;90m"),
-//    RED_BRIGHT("\u001b[0;91m"),
-//    GREEN_BRIGHT("\u001b[0;92m"),
-//    YELLOW_BRIGHT("\u001b[0;93m"),
-//    BLUE_BRIGHT("\u001b[0;94m"),
-//    PURPLE_BRIGHT("\u001b[0;95m"),
-//    CYAN_BRIGHT("\u001b[0;96m"),
-//    WHITE_BRIGHT("\u001b[0;97m"),
-//    BLACK_BOLD_BRIGHT("\u001b[1;90m"),
-//    RED_BOLD_BRIGHT("\u001b[1;91m"),
-//    GREEN_BOLD_BRIGHT("\u001b[1;92m"),
-//    YELLOW_BOLD_BRIGHT("\u001b[1;93m"),
-//    BLUE_BOLD_BRIGHT("\u001b[1;94m"),
-//    PURPLE_BOLD_BRIGHT("\u001b[1;95m"),
-//    CYAN_BOLD_BRIGHT("\u001b[1;96m"),
-//    WHITE_BOLD_BRIGHT("\u001b[1;97m"),
-//    BLACK_BACKGROUND_BRIGHT("\u001b[0;100m"),
-//    RED_BACKGROUND_BRIGHT("\u001b[0;101m"),
-//    GREEN_BACKGROUND_BRIGHT("\u001b[0;102m"),
-//    YELLOW_BACKGROUND_BRIGHT("\u001b[0;103m"),
-//    BLUE_BACKGROUND_BRIGHT("\u001b[0;104m"),
-//    PURPLE_BACKGROUND_BRIGHT("\u001b[0;105m"),
-//    CYAN_BACKGROUND_BRIGHT("\u001b[0;106m"),
-//    WHITE_BACKGROUND_BRIGHT("\u001b[0;107m");
-//
-//    override fun toString(): String {
-//        return value
-//    }
-//}
+class ANSI(var string: String = "") {
+    fun bold()              { string += "$ASCII_ESCAPE[1m" }
+    fun dim()               { string += "$ASCII_ESCAPE[2m" }
+    fun underline()         { string += "$ASCII_ESCAPE[4m" }
+    fun blink()             { string += "$ASCII_ESCAPE[5m" }
+    fun invert()            { string += "$ASCII_ESCAPE[7m" }
+    fun hide()              { string += "$ASCII_ESCAPE[8m" }
+    fun defaultForeground() { string += "$ASCII_ESCAPE[39m" }
+    fun defaultBackground() { string += "$ASCII_ESCAPE[49m" }
+
+    fun reset()          { string += "$ASCII_ESCAPE[0m" }
+    fun resetBold()      { string += "$ASCII_ESCAPE[21m" }
+    fun resetDim()       { string += "$ASCII_ESCAPE[22m" }
+    fun resetUnderline() { string += "$ASCII_ESCAPE[24m" }
+    fun resetBlink()     { string += "$ASCII_ESCAPE[25m" }
+    fun resetInvert()    { string += "$ASCII_ESCAPE[27m" }
+    fun resetHide()      { string += "$ASCII_ESCAPE[28m" }
+    override fun toString() = string
+}
+
+fun String.ansi(block: ANSI.() -> Unit) = ANSI(this).apply(block)
+
+val x = "".ansi {  }
