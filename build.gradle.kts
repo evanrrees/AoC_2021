c0c48f76b405564d63fabc5aaf6e3c6e0ee3f4be
err87 <err87@cornell.edu>
2021-12-16 12:39:18 -0500

    add tinylog dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,6 +11,7 @@ repositories {
 }
 
 dependencies {
+    implementation("org.tinylog:tinylog-api-kotlin:2.4.1")
     implementation("org.jetbrains.kotlin:kotlin-script-runtime:1.6.0")
     implementation("org.jetbrains.kotlin:kotlin-stdlib:1.6.0")
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
